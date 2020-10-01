33ab3f70c77afc68a4496e9e90f7198a1f7ee079
Danielle Arquisola <64483418+daniellearquisola@users.noreply.github.com>
2020-10-01 15:18:29 -0500

    feat: created counter component

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { render } from "react-dom";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import Counter from "./Components/Counter";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Drink a glass of water!</Text>
+      {/* <Button
+        onPress={() => {
+          alert("We did it!");
+        }}
+        title="Add Water"
+      /> */}
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Components/Counter.js
@@ -0,0 +1,42 @@
+import { render } from "react-dom";
+import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+export default class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  onIncrement = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
+  onSubtract = () => {
+    if (this.state.counter > 0) {
+      this.setState({
+        counter: this.state.counter - 1,
+      });
+    }
+  };
+
+  render() {
+    let counter = this.state.counter;
+    return (
+      <View>
+        <Text>Counter: {counter}</Text>
+        <TouchableOpacity onPress={this.onIncrement}>
+          <Icon name="plus" size={20} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.onSubtract}>
+          <Icon name="minus" size={20} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
