65c7c65ff67c62cda2b024c3c2ba37607b3079c5
Danielle Arquisola <64483418+daniellearquisola@users.noreply.github.com>
2020-10-01 15:27:30 -0500

    chore: added counter.js

--- Components/Counter.js
+++ Components/Counter.js
@@ -40,3 +40,12 @@ export default class Counter extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
